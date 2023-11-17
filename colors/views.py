4df923b0c36d61e5c0c689b3d6704b063e8369eb
OrdinaryAzazel <19622052@mahasiswa.itb.ac.id>
2023-11-17 23:51:25 +0700

    update views

--- colors/views.py
+++ colors/views.py
@@ -8,6 +8,8 @@ from .cbircolor import colormethod
 from PIL import Image
 import cv2
 import os
+import time
+import base64
 
 class ImgageProcessing(View):
     # def read_images_from_folder(folder_path):
@@ -65,8 +67,17 @@ class ImgageProcessing(View):
         main_image_path = os.path.join(main_imagefolder, jpg_file)
         dataset_folder = os.path.join(settings.MEDIA_ROOT, 'dataset/')
 
+        start_time = time.time()
         processed_images = self.process_images(main_image_path, dataset_folder)
-        return JsonResponse({'processed_images': processed_images})
+        end_time = time.time()
+        total_time = end_time - start_time
+
+        response_data = {
+            'processed_images': processed_images,
+            'total_time': total_time
+        }
+
+        return JsonResponse(response_data)
 
     def process_images(self, main_image_path, dataset_folder):
         processed_images = []
@@ -75,7 +86,13 @@ class ImgageProcessing(View):
             # Perform image processing and calculate percentage
             percentage = self.calculate_percentage(main_image_path, dataset_image_path)
             # Save the processed image to the database
-            processed_images.append({'image_name': image_name, 'percentage': percentage})
+            with open(dataset_image_path, "rb") as image_file:
+                encoded_image = base64.b64encode(image_file.read()).decode('utf-8')
+            if percentage>=0.60:
+                processed_images.append({
+                    'image_name': image_name, 
+                    'percentage': percentage,
+                    'image_data': ''})#harusnya encoded_image, tapi tidak dibuat karena belum ada frontend
         return processed_images
 
 
