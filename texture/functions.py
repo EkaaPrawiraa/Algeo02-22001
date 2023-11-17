bc31daef7c955bde1bd4046b4eb47f592530e61e
frdmmm <faridm0741@gmail.com>
2023-11-17 15:47:09 +0700

    Updata texture/functions

--- texture/functions.py
+++ texture/functions.py
@@ -1,60 +1,65 @@
 import cv2
 import numpy as np
-def rgbtograyscale(rgb):
-    return round(0.29*rgb[0]+0.587*rgb[1]+0.114*rgb[2])
-def transpose_matrix(matrix):
-    return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
-def makecooccurance(path):
-    img=cv2.imread(path)
+
+def rgb_to_grayscale(rgb):
+    return np.round(0.29 * rgb[..., 0] + 0.587 * rgb[..., 1] + 0.114 * rgb[..., 2])
+
+def make_cooccurrence(path):
+    img = cv2.imread(path)
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    height, width, _=img.shape
+
+    height, width, _ = img.shape
     matrix_size = (256, 256)
     matrix = np.zeros(matrix_size)
+
+    grayscale_img = rgb_to_grayscale(img_rgb)
+
     for i in range(height):
-        for j in range(width-1):
-            x=rgbtograyscale(img_rgb[i,j])
-            y=rgbtograyscale(img_rgb[i,j+1])
-            matrix[x,y]+=1
-    matrix=np.add(matrix, transpose_matrix(matrix))
-    total=np.sum(matrix)
-    comatrix=np.divide(matrix, total)
+        for j in range(width - 1):
+            x = int(grayscale_img[i, j])
+            y = int(grayscale_img[i, j + 1])
+            matrix[x, y] += 1
+
+    matrix += matrix.T  # Transpose in-place
+    total = np.sum(matrix)
+    comatrix = matrix / total
     return comatrix
 
 def contrast(comatrix):
-    hasil=0
-    for i in range(256):
-        for j in range(256):
-            hasil+=comatrix[i,j]*((i-j)**2)
-    return hasil
+    i, j = np.ogrid[:256, :256]
+    return np.sum(comatrix * ((i - j) ** 2))
+
 def homogeneity(comatrix):
-    hasil=0
-    for i in range(256):
-        for j in range(256):
-            hasil += comatrix[i, j] / (1 + (i - j) ** 2)
-    return hasil
+    i, j = np.ogrid[:256, :256]
+    return np.sum(comatrix / (1 + (i - j) ** 2))
+
 def entropy(comatrix):
     epsilon = 1e-10
     comatrix[comatrix == 0] = epsilon
-    entropy = -np.sum(comatrix * np.log(comatrix))
-    return entropy
+    return -np.sum(comatrix * np.log(comatrix))
 
 def cosine_similarity(vect1, vect2):
     dot_product = np.dot(vect1, vect2)
     norm_vect1 = np.linalg.norm(vect1)
     norm_vect2 = np.linalg.norm(vect2)
-    
     similarity = dot_product / (norm_vect1 * norm_vect2)
-    
     return similarity
-def makevector(path):
-    comatrix=makecooccurance(path)
-    print(comatrix)
-    vector=np.array([contrast(comatrix), homogeneity(comatrix), entropy(comatrix)])
+
+def make_vector(path):
+    comatrix = make_cooccurrence(path)
+    vector = np.array([contrast(comatrix), homogeneity(comatrix), entropy(comatrix)])
     return vector
-matrix=np.array([[1,2,3],
-                [4,5,6],
-                [7,8,9]])
-matrix=np.add(matrix, transpose_matrix(matrix))
-total=np.sum(matrix)
-comatrix=np.divide(matrix, total)
-print (comatrix)
+
+def convert_to_grayscale(image_path, output_path):
+    img = cv2.imread(image_path)
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite(output_path, gray_img)
+
+# Example usage
+path1 = '../media/dataset/messageImage_1669391260132.jpg'
+path2 = '402773.jpg'
+vector1 = make_vector(path1)
+vector2 = make_vector(path2)
+similarity = cosine_similarity(vector1, vector2)
+
+print(similarity)
