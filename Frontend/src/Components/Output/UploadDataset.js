73acae642fddd802e7b790b4e9a72840c17b41dd
OrdinaryAzazel <19622052@mahasiswa.itb.ac.id>
2023-11-18 17:54:56 +0700

    tests

--- Frontend/src/Components/Output/UploadDataset.js
+++ Frontend/src/Components/Output/UploadDataset.js
@@ -1,47 +1,58 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-const { ipcRenderer } = window.require('electron');
 
-function App() {
-  const [selectedFolder, setSelectedFolder] = useState('');
-
-  const openFolderDialog = () => {
-    ipcRenderer.send('select-folder');
+// FolderUploadButton
+const FolderUploadButton = ({ onFolderSelect, onFolderUpload }) => {
+  const handleSelect = (event) => {
+    const folder = event.target.files[0];
+    onFolderSelect(folder);
   };
 
-  ipcRenderer.on('selected-folder', (event, folderPath) => {
-    setSelectedFolder(folderPath);
-  });
+  const handleUpload = () => {
+    onFolderUpload();
+  };
 
-  const uploadFolder = () => {
-    if (selectedFolder) {
+  return (
+    <div>
+      <input type="file" id="folderInput" webkitdirectory="" directory="" onChange={handleSelect} />
+      <button onClick={handleUpload}>Upload Folder</button>
+    </div>
+  );
+};
+
+// YourParentComponent
+const YourParentComponent = () => {
+  const [selectedFolder, setSelectedFolder] = useState(null);
+
+  const handleFolderSelect = (folder) => {
+    setSelectedFolder(folder);
+  };
+
+  const handleFolderUpload = async () => {
+    try {
       const formData = new FormData();
-      formData.append('folder', selectedFolder);
+      formData.append('file', selectedFolder);
 
-      axios.post('http://127.0.0.1:8000/api/upload/folder/', formData, {
+      // Kirim request ke backend Anda
+      const response = await axios.post('http://localhost:8000/api/upload/folder/', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
-      })
-      .then(response => {
-        console.log(response.data);
-        // Handle the server response as needed
-      })
-      .catch(error => {
-        console.error('Error uploading folder:', error);
       });
-    } else {
-      console.error('No folder selected');
+
+      // Handle respons jika diperlukan
+      console.log('Upload berhasil', response.data);
+    } catch (error) {
+      // Handle error
+      console.error('Error mengupload folder', error.response.data);
     }
   };
 
   return (
     <div>
-      <button onClick={openFolderDialog}>Select Folder</button>
-      <p>Selected Folder: {selectedFolder}</p>
-      <button onClick={uploadFolder}>Upload Folder</button>
+      <FolderUploadButton onFolderSelect={handleFolderSelect} onFolderUpload={handleFolderUpload} />
     </div>
   );
-}
+};
 
-export default App;
+export default YourParentComponent;
